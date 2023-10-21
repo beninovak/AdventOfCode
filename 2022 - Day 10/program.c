317091c621f075ccae1ac4ef13735a9811432090
beninovak <beni.novak13@gmail.com>
2023-10-21 17:48:52 +0200

    2022 - Day 10 part 1

---
+++ 2022 - Day 10/program.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int addToTotal(int cycle);
+
+int main() {
+
+    FILE *fptr;
+
+    fptr = fopen("C:\\Users\\Janez Povezava\\CLionProjects\\AdventOfCode\\2022 - Day 10\\input.txt", "r");
+
+    int X = 1;
+    int amount = 0;
+    int strength = 0;
+    int totalStrength = 0;
+    int cycle = 0;
+    char line[10];
+
+    // Read the content of file line by line
+    while(fgets(line, 10, fptr)) {
+
+        bool isNoop = (strcmp(line, "noop") == 1);
+
+        if (isNoop) {
+            cycle++;
+            strength = cycle * X;
+        } else {
+
+            cycle++;
+            strength = cycle * X;
+
+            if (addToTotal(cycle) == 1)  {
+                printf("Strength at cycle %d: %d\n", cycle, strength);
+                totalStrength += strength;
+            }
+
+            cycle++;
+            strength = cycle * X;
+
+            char *token = strtok(line, " ");
+            while (token != NULL) {
+
+                token = strtok(NULL, " ");
+                amount = atoi(token);
+
+                if (amount != 0) {
+                    X += amount;
+                }
+            }
+            printf("Strength at cycle %d: %d\n", cycle, strength);
+        }
+
+        if (addToTotal(cycle) == 1)  {
+            totalStrength += strength;
+        }
+    }
+
+    printf("Sum of signal strengths: %d", totalStrength);
+
+    fclose(fptr);
+    return 0;
+}
+
+int addToTotal(int cycle) {
+
+    if ((cycle == 20) || ((cycle - 20) % 40 == 0)) {
+        printf("Adding to total at cycle %d: ", cycle);
+        return 1;
+    }
+
+    return 0;
+}
