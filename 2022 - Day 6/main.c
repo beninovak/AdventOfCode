63b9617fb5aa113ea839d314ff598a8f7056202d
beninovak <beni.novak13@gmail.com>
2023-10-31 18:14:23 +0100

    2022 - Day 6 part 2

--- 2022 - Day 6/main.c
+++ 2022 - Day 6/main.c
@@ -1,33 +1,57 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
     FILE *fptr = fopen("C:\\Users\\Janez Povezava\\CLionProjects\\AdventOfCode\\2022 - Day 6\\input.txt", "r");
-    char letter, lastFour[5] = "\0\0\0\0\0";
+    char letter, lastFour[5] = "", message[15] = "";
     int counter = 1;
-    int containsDuplicate;
+    int markerFound = 0, markerContainsDuplicate, messageContainsDuplicate;
 
     while ((letter = getc(fptr)) != EOF) {
 
-        containsDuplicate = 0;
-        lastFour[0] = lastFour[1];
-        lastFour[1] = lastFour[2];
-        lastFour[2] = lastFour[3];
+        markerContainsDuplicate = 0;
+        messageContainsDuplicate = 0;
+
+        for (int i = 0; i < 4; i++) {
+            lastFour[i] = lastFour[i + 1];
+        }
         lastFour[3] = letter;
+
+        for (int i = 0; i < 14; i++) {
+            message[i] = message[i + 1];
+        }
+        message[13] = letter;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 3; j > i; j--) {
                 if (lastFour[i] == lastFour[j]) {
-                    containsDuplicate = 1;
+                    markerContainsDuplicate = 1;
                     break;
                 }
             }
-            if (containsDuplicate == 1) break;
+            if (markerContainsDuplicate == 1) break;
         }
 
-        if (counter >= 4 && containsDuplicate == 0) {
+        for (int i = 0; i < 13; i++) {
+            for (int j = 13; j > i; j--) {
+                if (message[i] == message[j]) {
+                    messageContainsDuplicate = 1;
+                    break;
+                }
+            }
+            if (messageContainsDuplicate == 1) break;
+        }
+
+        if (markerFound == 0 && counter >= 4 && markerContainsDuplicate == 0) {
             printf("First marker is complete after: %d characters.\n", counter);
-            printf("The marker is: %s", lastFour);
+            printf("The marker is: %s\n", lastFour);
+            markerFound = 1;
+        }
+
+        if (counter >= 14 && messageContainsDuplicate == 0) {
+            printf("First message is complete after: %d characters.\n", counter);
+            printf("The message is: %s\n", message);
             break;
         }
 
