4072ec84c4b0328e22ee937fd59849a17d9684c7
beninovak <beni.novak13@gmail.com>
2023-11-11 20:51:39 +0100

    2022 - Day 11 part 2...long long for days

--- 2022 - Day 11/program.c
+++ 2022 - Day 11/program.c
@@ -9,25 +9,25 @@ enum OPERATION {
     MULTIPLY
 };
 
-struct Monkey {
+typedef struct _Monkey {
     int testDivisibleBy;
     int testTrueMonkeyId;
     int testFalseMonkeyId;
     enum OPERATION operation;
     int operationValue;
-    int inspectedItems;
+    long long inspectedItems;
     long long items[50];
-};
+} Monkey;
 
-void swap(long* xp, long* yp)
+void swap(long long* xp, long long* yp)
 {
-    int temp = *xp;
+    long long temp = *xp;
     *xp = *yp;
     *yp = temp;
 }
 
 // Selection sort
-void sortArrayDesc(long arr[], int n)
+void sortArrayDesc(long long arr[], int n)
 {
     int i, j, min_idx;
 
@@ -51,18 +51,89 @@ int main() {
     long long worryLevel;
 
     // Starting monkey values. If operationValue is 0, that means the old worryLevel should be multiplied by itself.
-    struct Monkey Monkey0 = { 3, 7, 4, MULTIPLY, 5, 0, { 66, 71, 94 } };
-    struct Monkey Monkey1 = { 17, 3, 0, ADD, 6, 0, { 70 } };
-    struct Monkey Monkey2 = { 2, 3, 1, ADD, 5, 0, { 62, 68, 56, 65, 94, 78 } };
-    struct Monkey Monkey3 = { 19, 7, 0, ADD, 2, 0, { 89, 94, 94, 67 } };
-    struct Monkey Monkey4 = { 11, 5, 6, MULTIPLY, 7, 0, { 71, 61, 73, 65, 98, 98, 63 } };
-    struct Monkey Monkey5 = { 5, 2, 1, ADD, 7, 0, { 55, 62, 68, 61, 60 } };
-    struct Monkey Monkey6 = { 13, 5, 2, ADD, 1, 0, { 93, 91, 69, 64, 72, 89, 50, 71 } };
-    struct Monkey Monkey7 = { 7, 4, 6, MULTIPLY, 0, 0, { 76, 50 } };
+    Monkey Monkey0 = {
+            3,
+            7,
+            4,
+            MULTIPLY,
+            5,
+            0,
+            { 66, 71, 94 }
+    };
+
+    Monkey Monkey1 = {
+            17,
+            3,
+            0,
+            ADD,
+            6,
+            0,
+            { 70 }
+    };
+
+    Monkey Monkey2 = {
+            2,
+            3,
+            1,
+            ADD,
+            5,
+            0,
+            { 62, 68, 56, 65, 94, 78 }
+    };
+
+    Monkey Monkey3 = {
+            19,
+            7,
+            0,
+            ADD,
+            2,
+            0,
+            { 89, 94, 94, 67 }
+    };
+
+    Monkey Monkey4 = {
+            11,
+            5,
+            6,
+            MULTIPLY,
+            7,
+            0,
+            { 71, 61, 73, 65, 98, 98, 63 }
+    };
+
+    Monkey Monkey5 = {
+            5,
+            2,
+            1,
+            ADD,
+            7,
+            0,
+            { 55, 62, 68, 61, 60 }
+    };
+
+    Monkey Monkey6 = {
+            13,
+            5,
+            2,
+            ADD,
+            1,
+            0,
+            { 93, 91, 69, 64, 72, 89, 50, 71 }
+    };
+
+    Monkey Monkey7 = {
+            7,
+            4,
+            6,
+            MULTIPLY,
+            0,
+            0,
+            { 76, 50 }
+    };
 
     int monkeyCount = 8;
-    struct Monkey monkeys[8] = { Monkey0, Monkey1, Monkey2, Monkey3, Monkey4, Monkey5, Monkey6, Monkey7 };
-    long monkeyInspections[8];
+    Monkey monkeys[8] = { Monkey0, Monkey1, Monkey2, Monkey3, Monkey4, Monkey5, Monkey6, Monkey7 };
+    long long monkeyInspections[8];
 
     long LCM = 1;
 
@@ -73,7 +144,7 @@ int main() {
     printf("LCM: %ld\n", LCM);
 
     // 20 Rounds
-    for (int i = 0; i < 3776; i++) {
+    for (int i = 0; i < 10000; i++) {
 
         for (int j = 0; j < monkeyCount; j++) {
 
@@ -96,7 +167,6 @@ int main() {
                         break;
 
                     case MULTIPLY:
-
                         if (monkeys[j].operationValue == 0) {
                             worryLevel *= worryLevel;
                         } else {
@@ -105,10 +175,7 @@ int main() {
                         break;
                 }
 
-//                worryLevel = (int)floorf((float)worryLevel / 3);
-                printf("Worry before: %lld\n", worryLevel);
                 worryLevel = worryLevel % LCM;
-                printf("Worry after: %lld\n", worryLevel);
 
                 // Monkey tests worry level
                 int monkeyToThrowToId;
@@ -135,39 +202,21 @@ int main() {
                 monkeys[j].items[k] = 0;
             }
         }
-
-//        if ((i + 1) % 5 == 0) {
-            printf("\nResults after round %d:\n", (i + 1));
-
-            for (int n = 0; n < monkeyCount; n++) {
-
-                long long *finalItems = monkeys[n].items;
-                printf("Monkey %d final items: ", n);
-
-                for (int q = 0; q < 10; q++) {
-                    printf("%lld ", finalItems[q]);
-                }
-
-                printf("\n");
-            }
-            printf("\n-------------------------------------------\n");
-//        }
     }
-    // ------------ Rounds finished ----------------
 
     for (int n = 0; n < monkeyCount; n++) {
         monkeyInspections[n] = monkeys[n].inspectedItems;
-        printf("Monkey %d inspected %ld items \n", n, monkeyInspections[n]);
+        printf("Monkey %d inspected %lld items \n", n, monkeyInspections[n]);
     }
 
     sortArrayDesc(monkeyInspections, monkeyCount);
 
     for (int o = 0; o < monkeyCount; o++) {
-        printf("%ld ", monkeyInspections[o]);
+        printf("%lld ", monkeyInspections[o]);
     }
 
-    printf("\n1st place Monkey: %ld: --- 2nd place Monkey: %ld\n", monkeyInspections[0], monkeyInspections[1]);
-    long n     monkeyBusiness = monkeyInspections[0] * monkeyInspections[1];
+    printf("\n1st place Monkey: %lld: --- 2nd place Monkey: %lld\n", monkeyInspections[0], monkeyInspections[1]);
+    long long monkeyBusiness = monkeyInspections[0] * monkeyInspections[1];
 
     printf("\nMonkey business: %lld\n", monkeyBusiness);
 
