6a487afef304c08d3d6a28a4cf746dfa10f44a5b
beninovak <beninonovakino@proton.me>
2025-05-27 20:50:48 +0200

    2024 Day 2 - Moved logic to another function and added gf2

--- 2024 - Day 2/main.c
+++ 2024 - Day 2/main.c
@@ -1,77 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../../C-headers/read_file.h"
+
+int get_diff(int a, int b) {
+    int diff = a - b;
+    return diff < 0 ? diff * -1 : diff;
+}
+
+int is_line_unsafe(char* line) {
+    char* token;
+    int previous = 0, current = 0, diff = 0, direction = 0;
+
+    int unsafe, count;
+
+    count = 0;
+    unsafe = 0;
+    token = strtok(line, " \n");
+    while(token != NULL) {
+        current = atoi(token);
+        printf("%d ", current);
+
+        if (count == 0) {
+            previous = current;
+            token = strtok(NULL, " \n");
+            count++;
+            continue;
+        }
+
+        int diff = current - previous;
+        if (diff < 0) {
+            diff = diff * -1;
+        }
+        if (diff < 1 || diff > 3) {
+            printf(" - Big diff");
+            unsafe = 1;
+            break; 
+        }
+
+        if (count > 1) {
+            if ((direction == -1 && current > previous) || 
+                (direction == 1 && current < previous)
+            ) {
+                printf(" - Wrong direction");
+                unsafe = 1;
+                break;
+            }
+
+        }
+
+        if (current < previous) {
+            direction = -1;
+        } else {
+            direction = 1;
+        }
+
+                     
+        previous = current;
+        token = strtok(NULL, " \n");
+        count++;
+    }
+    printf("\n");
+    return unsafe;
+}
 
 int main () {
 
-    FILE* fptr = fopen("input.txt", "rb");
+    FILE* fptr = fopen("example.txt", "rb");
     if (fptr == NULL) {
         printf("Cannot open file!\n");
         return 1;
     }
     
-    char line[64] = "";
-    char* token;
-    int previous = 0, current = 0;
+    _file* file = file_read(fptr);
+
     int safe_reports = 0;
-    int direction = 0; // -1 if decreasing, 1 if increasing
-    int count;
-    int unsafe;
-
-    while(fgets(line, sizeof(line), fptr)) {
-
-        printf("\n");
-        count = 0;
-        unsafe = 0;
-        token = strtok(line, " \n");
-        while(token != NULL) {
-            current = atoi(token);
-            printf("%d ", current);
-
-            if (count == 0) {
-                previous = current;
-                token = strtok(NULL, " \n");
-                count++;
-                continue;
-            }
-
-            int diff = current - previous;
-            if (diff < 0) {
-                diff = diff * -1;
-            }
-            if (diff < 1 || diff > 3) {
-                printf(" - Big diff");
-                unsafe = 1;
-                break; 
-            }
-
-            if (count > 1) {
-                if ((direction == -1 && current > previous) || 
-                    (direction == 1 && current < previous)
-                ) {
-                    printf(" - Wrong direction");
-                    unsafe = 1;
-                    break;
-                }
-
-            }
-
-            if (current < previous) {
-                direction = -1;
-            } else {
-                direction = 1;
-            }
-
-                         
-            previous = current;
-            token = strtok(NULL, " \n");
-            count++;
-        }
-        if (unsafe == 0) {
-            safe_reports++;
+    for(int i = 0; i < file->line_count; i++) {
+        if(is_line_unsafe(file->lines[i]->start) == 0) {
+            safe_reports++; 
+        } else {
+            // TODO - Split line into
         }
     }
-
+    
     printf("\nSafe reports: %d\n", safe_reports);
     return 0;
 }
