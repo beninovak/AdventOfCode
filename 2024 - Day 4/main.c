5ede924fb7f5e5be705d0901ebff44dccc3ec7f1
beninovak <beninonovakino@proton.me>
2025-06-02 20:42:34 +0200

    2024 Day 4 part 2

--- 2024 - Day 4/main.c
+++ 2024 - Day 4/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include "../../C-headers/read_file.h"
 
 int main() {
@@ -39,59 +38,72 @@ int main() {
     int too_close_to_left, too_close_to_right, too_close_to_top, too_close_to_bottom;
     for(int r = 0; r < rows; r++) {
         for(int c = 0; c < columns; c++) {
-            if (grid[r][c] == 'X') {
+            if (grid[r][c] == 'M') {
                 printf("Found x: [%d, %d]\n", r, c);
                 too_close_to_left = too_close_to_right = too_close_to_top = too_close_to_bottom = 0;
-                if (r - 3 < 0) {
+                if (r - 2 < 0) {
                     too_close_to_top = 1;
                 }
-                if (r + 3 > rows - 1) {
+                if (r + 2 > rows - 1) {
                     too_close_to_bottom = 1;
                 }
-                if (c - 3 < 0) {
+                if (c - 2 < 0) {
                     too_close_to_left = 1;
                 }
-                if (c + 3 > columns - 1) {
+                if (c + 2 > columns - 1) {
                     too_close_to_right = 1;
                 }
 
-                // Check top
-                if (too_close_to_top == 0 && grid[r-1][c] == 'M' && grid[r-2][c] == 'A' && grid[r-3][c] == 'S') {
-                    xmas_count++;
-                }
-
-                // Check bottom
-                if (too_close_to_bottom == 0 && grid[r+1][c] == 'M' && grid[r+2][c] == 'A' && grid[r+3][c] == 'S') {
-                    xmas_count++;
-                }
-
-                if (too_close_to_left == 0) {
-                    if (grid[r][c-1] == 'M' && grid[r][c-2] == 'A' && grid[r][c-3] == 'S') {
-                        xmas_count++;
-                    }
-                    if (too_close_to_top == 0 && grid[r-1][c-1] == 'M' && grid[r-2][c-2] == 'A' && grid[r-3][c-3] == 'S') {
-                        xmas_count++;
-                    }
-                    if (too_close_to_bottom == 0 && grid[r+1][c-1] == 'M' && grid[r+2][c-2] == 'A' && grid[r+3][c-3] == 'S') {
+                if (too_close_to_left == 0 && too_close_to_top == 0) {
+                    char bottom_left_char = grid[r][c-2];
+                    char top_right_char = grid[r-2][c];
+                    if (
+                        !(bottom_left_char == top_right_char ||
+                        grid[r-1][c-1] != 'A' || grid[r-2][c-2] != 'S' || 
+                        (bottom_left_char != 'S' && bottom_left_char != 'M') || (top_right_char != 'S' && top_right_char != 'M')
+                    )) {
                         xmas_count++;
                     }
                 }
-
-                if (too_close_to_right == 0) {
-                    if (grid[r][c+1] == 'M' && grid[r][c+2] == 'A' && grid[r][c+3] == 'S') {
-                        xmas_count++;
-                    }
-                    if (too_close_to_top == 0 && grid[r-1][c+1] == 'M' && grid[r-2][c+2] == 'A' && grid[r-3][c+3] == 'S') {
-                        xmas_count++;
-                    }
-                    if (too_close_to_bottom == 0 && grid[r+1][c+1] == 'M' && grid[r+2][c+2] == 'A' && grid[r+3][c+3] == 'S') {
+                
+                if (too_close_to_left == 0 && too_close_to_bottom == 0) {
+                    char top_left_char = grid[r][c-2];
+                    char bottom_right_char = grid[r+2][c];
+                    if (
+                        !(bottom_right_char == top_left_char ||
+                        grid[r+1][c-1] != 'A' || grid[r+2][c-2] != 'S' || 
+                        (bottom_right_char != 'S' && bottom_right_char != 'M') || (top_left_char != 'S' && top_left_char != 'M')
+                    )) {
                         xmas_count++;
                     }
                 }
 
+                if (too_close_to_right == 0 && too_close_to_top == 0) {
+                    char top_left_char = grid[r-2][c];
+                    char bottom_right_char = grid[r][c+2];
+                    if (
+                        !(bottom_right_char == top_left_char ||
+                        grid[r-1][c+1] != 'A' || grid[r-2][c+2] != 'S' || 
+                        (bottom_right_char != 'S' && bottom_right_char != 'M') || (top_left_char != 'S' && top_left_char != 'M')
+                    )) {
+                        xmas_count++;
+                    }
+                }
+
+                if (too_close_to_right == 0 && too_close_to_bottom == 0) {
+                    char bottom_left_char = grid[r+2][c];
+                    char top_right_char = grid[r][c+2];
+                    if (
+                        !(top_right_char == bottom_left_char ||
+                        grid[r+1][c+1] != 'A' || grid[r+2][c+2] != 'S' || 
+                        (top_right_char != 'S' && top_right_char != 'M') || (bottom_left_char != 'S' && bottom_left_char != 'M')
+                    )) {
+                        xmas_count++;
+                    }
+                }
             }
         }
     }
-    printf("XMAS COUNT: %d\n", xmas_count);
+    printf("XMAS COUNT: %d\n", xmas_count / 2); // Divided by 2 cuz otherwise every X-MAS is counted twice.
     return 0;
 }
