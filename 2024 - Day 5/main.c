fa158cd4ae0040cfe36f81282b0be1f8f270bbc1
beninovak <beninonovakino@proton.me>
2025-06-04 23:59:52 +0200

    2024 Day 5 part 2 WIP...almost

--- 2024 - Day 5/main.c
+++ 2024 - Day 5/main.c
@@ -6,7 +6,7 @@
 
 int main() {
 
-    FILE* fptr = fopen("input.txt", "rb");
+    FILE* fptr = fopen("example.txt", "rb");
     _file* file = file_read(fptr);
     fclose(fptr);
 
@@ -24,25 +24,18 @@ int main() {
     for(int i = 0; i < continue_from - 1; i++) {
         page_order_rules[i] = calloc(2, sizeof(int));
         token = strtok(file->lines[i]->start, "|");
-        // printf("%s - ", token);
         page_order_rules[i][0] = atoi(token);
         token = strtok(NULL, "\n");
-        // printf("%s\n", t->tokens[j]oken);
         page_order_rules[i][1] = atoi(token);
         token = NULL;
     }
 
-    long update_sums = 0;
+    long corrent_updates_sum = 0;
+    long incorrent_updates_sum = 0;
     int token_rows = file->line_count - continue_from;
     _tokens** tokens = (_tokens**)calloc(token_rows, sizeof(_tokens*));
     for(int i = 0; i < token_rows; i++) {
         tokens[i] = _strings_tokenize(file->lines[continue_from + i]->start, ',');
-
-        // for (int j = 0; j < tokens[i]->size; j++) {
-        //     // printf("%s ", tokens[i]->tokens[j]);
-        //     printf("%s\n", tokens[i]->tokens[j]);
-        // }
-        // printf("\n");
         
         int is_valid = 1;
         for(int t = 0; t < tokens[i]->size - 1; t++) {
@@ -51,16 +44,25 @@ int main() {
 
             for(int p = 0; p < continue_from - 1; p++) {
                 if (page_order_rules[p][1] == num1 && page_order_rules[p][0] == num2) { // If numbers are in the wrong order
-                    is_valid = 0;                        
-                    break;
+                    is_valid = 0;
+                    // TODO - reorder all incorrect updates, not just two tokens at a time. qsort??
+                    //break;
+                    // char* temp = calloc(tokens[i]->size, sizeof(char));
+                    // strcpy(temp, tokens[i]->tokens[t]);
+                    // strcpy(tokens[i]->tokens[t], tokens[i]->tokens[t+1]);
+                    // strcpy(tokens[i]->tokens[t+1], temp);
+                    // free(temp);
                 }
             }
-            if (is_valid == 0) break;
+            // if (is_valid == 0) break;
         }
 
         if (is_valid == 1) {
-            printf("%d - %s\n", (tokens[i]->size / 2) + 1, tokens[i]->tokens[(tokens[i]->size / 2)]);
-            update_sums += atoi(tokens[i]->tokens[(tokens[i]->size / 2)]); // Add middle tokens
+            printf("Corrent: %d - %s\n", (tokens[i]->size / 2) + 1, tokens[i]->tokens[(tokens[i]->size / 2)]);
+            corrent_updates_sum += atoi(tokens[i]->tokens[(tokens[i]->size / 2)]); // Add middle tokens
+        } else {
+            printf("Incorrect: %d - %s\n", (tokens[i]->size / 2) + 1, tokens[i]->tokens[(tokens[i]->size / 2)]);
+            incorrent_updates_sum += atoi(tokens[i]->tokens[(tokens[i]->size / 2)]); // Add middle tokens
         }
         _tokens_free(tokens[i]);
     }
@@ -68,6 +70,7 @@ int main() {
 
     _file_free(file);
 
-    printf("\nSum: %lu\n", update_sums);
+    printf("\nCorrect sum: %lu\n", corrent_updates_sum);
+    printf("\nIncorrect sum: %lu\n", incorrent_updates_sum);
     return 0;
 }
