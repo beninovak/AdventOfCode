2c4c6ebb2b2cabf8e7288de073dc7ba07d5cf913
beninovak <beninonovakino@proton.me>
2025-06-19 16:18:45 +0200

    Day 7 WIP

---
+++ 2024 - Day 7/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../C-headers/_strings.h"
+#include "../../C-headers/read_file.h"
+#include <stdint.h>
+
+int is_doable(_tokens* toks, uint64_t target, uint32_t last_index) {
+    uint32_t last = atoi(toks->tokens[last_index]);
+    printf("Checking target: %lu, last: %u", target, last);
+    if (target % last != 0) {
+        if (last_index > 1) {
+            printf(" -> Not divisible, checking subtraction!\n");
+            return is_doable(toks, target - last, last_index - 1);
+        } else {
+            // if (target == last) {
+            //     printf(" -> Last not divisible and good!\n");
+            //     return 1;
+            // }
+            printf(" -> Not divisible, returning cuz last!\n");
+            return 0;
+        }
+    } else if (last_index > 1) {
+        printf(" -> Divisible and not done!\n");
+        return is_doable(toks, target / last, last_index - 1);
+    } else if (target == last) {
+        printf(" -> Last and good!\n");
+        return 1;
+    } else {
+        printf(" -> Divisible and returning cuz last!\n");
+        return 0;
+    }
+}
+
+int main() {
+
+    FILE* fptr = fopen("example.txt", "rb");
+    _file* file = file_read(fptr);
+    fclose(fptr);
+    
+    char* ch;
+    uint64_t total = 0;
+    uint64_t target;
+    uint32_t last;
+
+    for(int i = 0; i < file->line_count; i++) {
+        _tokens* toks = _strings_tokenize(file->lines[i]->start, ' ');
+        toks->tokens[0][strlen(toks->tokens[0]) - 1] = '\0'; // Removes ':' from the end of the first number.
+
+        target = atoi(toks->tokens[0]);
+
+        uint32_t last_index = toks->size - 1;
+
+        if (is_doable(toks, target, last_index)) {
+            total += target;
+        }
+        printf("\n---------------------- New total: %lu ----------------------\n\n", total);
+
+        _tokens_free(toks);
+    }
+
+    printf("\nTotal: %lu\n", total);
+    return 0;
+}
