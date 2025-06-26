c8ec3937ba7a74603f0203621859a626ff027285
beninovak <beninonovakino@proton.me>
2025-06-26 08:25:56 +0200

    day 7 part 2 better logic...less repetition

--- 2024 - Day 7/main.c
+++ 2024 - Day 7/main.c
@@ -16,77 +16,57 @@ int is_doable(int* numbers, uint64_t target, uint32_t last_index, FILE* output_f
         return target == last;
     }
 
+    int can_divide = 0, can_subtract = 0, check_subtract = 0, check_concat = 0;
     if (target % last == 0) {
         fprintf(output_file, " -> Divisible and not done: %lu / %d\n", target, last);
-
         if (!is_doable(numbers, target / last, last_index - 1, output_file)) {
-            if(!is_doable(numbers, target - last, last_index - 1, output_file)) {
-                char* last_str = calloc(20, sizeof(char));
-                snprintf(last_str, 20, "%d", last);
-                char* target_str = calloc(20, sizeof(char));
-                snprintf(target_str, 20, "%lu", target);
-
-                int last_len = strlen(last_str) - 1;
-                int target_ends_with_last = 1;
-                for (int i = strlen(target_str) - 1; i > 0; i--) {
-                    if (last_len < 0) break;
-                    if (target_str[i] != last_str[last_len]) {
-                        target_ends_with_last = 0;
-                        break;
-                    }
-                    last_len--;
-                }
-                if (target_ends_with_last == 0) {
-                    fprintf(output_file, " -> Checking concat. Target: %lu doesn't end with last: %d\n", target, last);
-                    return 0;
-                }
-
-                target_str[strlen(target_str) - strlen(last_str)] = '\0';
-                uint64_t new_target = atoll(target_str);
-                fprintf(output_file, " -> Checking concat. Target: %lu ends with last: %d. Remainder %lu\n", target, last, new_target);
-                int ret = is_doable(numbers, new_target, last_index - 1, output_file);
-                return ret;
+            if (!is_doable(numbers, target - last, last_index - 1, output_file)) {
+                check_concat = 1;
             }
-            return 1;
+            can_subtract = 1;
         }
-        return 1;
-    } else  {
+        can_divide = 1;
+    } else {
         fprintf(output_file, " -> Not divisible, checking subtraction %lu - %d\n", target, last);
-
         if (is_doable(numbers, target - last, last_index - 1, output_file)) {
             return 1;
         }
+        check_concat = 1;
     } 
-    char* last_str = calloc(20, sizeof(char));
-    snprintf(last_str, 20, "%d", last);
-    char* target_str = calloc(20, sizeof(char));
-    snprintf(target_str, 20, "%lu", target);
 
-    int last_len = strlen(last_str) - 1;
-    int target_ends_with_last = 1;
-    for (int i = strlen(target_str) - 1; i > 0; i--) {
-        if (last_len < 0) break;
-        if (target_str[i] != last_str[last_len]) {
-            target_ends_with_last = 0;
-            break;
+    if ((can_divide == 1 || can_subtract == 1) && check_concat == 0) return 1;
+
+    if (check_concat == 1) {
+        char* last_str = calloc(20, sizeof(char));
+        snprintf(last_str, 20, "%d", last);
+        char* target_str = calloc(20, sizeof(char));
+        snprintf(target_str, 20, "%lu", target);
+
+        int last_len = strlen(last_str) - 1;
+        int target_ends_with_last = 1;
+        for (int i = strlen(target_str) - 1; i > 0; i--) {
+            if (last_len < 0) break;
+            if (target_str[i] != last_str[last_len]) {
+                target_ends_with_last = 0;
+                break;
+            }
+            last_len--;
         }
-        last_len--;
-    }
-    if (target_ends_with_last == 0) {
-        fprintf(output_file, " -> Checking concat. Target: %lu doesn't end with last: %d\n", target, last);
-        return 0;
-    }
+        if (target_ends_with_last == 0) {
+            fprintf(output_file, " -> Checking concat. Target: %lu doesn't end with last: %d\n", target, last);
+            return 0;
+        }
 
-    target_str[strlen(target_str) - strlen(last_str)] = '\0';
-    uint64_t new_target = atoll(target_str);
-    fprintf(output_file, " -> Checking concat. Target: %lu ends with last: %d. Remainder %lu\n", target, last, new_target);
-    int ret = is_doable(numbers, new_target, last_index - 1, output_file);
-    return ret;
-
+        target_str[strlen(target_str) - strlen(last_str)] = '\0';
+        uint64_t new_target = atoll(target_str);
+        fprintf(output_file, " -> Checking concat. Target: %lu ends with last: %d. Remainder %lu\n", target, last, new_target);
+        return is_doable(numbers, new_target, last_index - 1, output_file);
+    }
     return 0;
 }
 
 int main() {
+    clock_t start_time = clock();
     FILE* fptr = fopen("input.txt", "r");
     
     char* token;
@@ -128,5 +108,8 @@ int main() {
     fprintf(debug_file, "\nTotal: %lu\n", total);
     printf("\nTotal: %lu\n", total);
     fclose(fptr);
+
+    double total_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+    printf("\nTime elapsed: %f\n", total_time);
     return 0;
 }
